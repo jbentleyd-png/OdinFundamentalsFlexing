71ad55bd48486f0d55be34b781892bb4afe03d08
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-02-22 11:20:27 -0700

    Added more JS.
    Added some date functions to add to my loading alert.

--- code.js
+++ code.js
@@ -1,3 +1,8 @@
 window.onload = function () {
-  alert("Welcome to the real world, homie.");
+  alert("Welcome to the real world, homie. The date is " + month + ".");
 };
+
+const fullDate = new Date();
+// Example output: "Sun Feb 22 2026 11:14:10 GMT-0700 (Mountain Standard Time)"
+
+const month = fullDate.toLocaleDateString(); //nifty little function. After learning string manipulation, can try to make it more manually just for fun.
